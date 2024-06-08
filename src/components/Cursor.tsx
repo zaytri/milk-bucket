b811276f2ad90277fc09388856a99df85219b03c
Zaytri <9072119+zaytri@users.noreply.github.com>
2024-06-08 16:00:18 -0700

    wip

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,1 +1,13 @@
-export default function Cursor() {}
+import { useState } from 'react'
+
+export default function Cursor() {
+  const [x, setX] = useState<number | null>(null)
+  const [y, setY] = useState<number | null>(null)
+
+  function onPointerMove(event: React.PointerEvent) {
+    setX(event.clientX)
+    setY(event.clientY)
+  }
+
+  function onPointerOut() {}
+}
